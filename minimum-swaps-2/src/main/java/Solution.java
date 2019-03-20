ca06b60351744f90d5daa79af720f1b673b98e48
Marcelo Cordeiro De Quadros <marcelo.quadros@livelo.com.br>
2019-03-20 00:05:37 -0300

    Added solution mininum-swaps and crush (array manipulation)

---
+++ minimum-swaps-2/src/main/java/Solution.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static int count = 0;
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int countSwaps = 0;
+        for (int i = 0; i < arr.length; i++){
+
+            if(arr[i]-1 != i){
+
+                int index = findCorrectSlot(arr, i, i+1);
+                swap(arr, i, index);
+                countSwaps++;
+            }
+        }
+
+        return countSwaps;
+    }
+
+    static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = j;
+        arr[j] = temp;
+    }
+
+    static int findCorrectSlot(int arr[], int start, int element){
+
+        for( int i = start; i < arr.length; i++){
+            if(arr[i] == element){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
